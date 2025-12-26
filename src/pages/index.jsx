54b8c201023efb22034b329da26ef25ceb275e83
sanie <bitakov73@mail.ru>
2025-12-26 22:17:19 +0300

    init: Vite + Minista project structure and base styles

---
+++ src/pages/index.jsx
@@ -0,0 +1,11 @@
+export const metadata = {
+    title: 'Home',
+}
+
+export default function () {
+  return (
+    <>
+        <h1>Главная</h1>
+    </>
+  )
+}
---
+++ src/pages/movies.jsx
@@ -0,0 +1,12 @@
+export const metadata = {
+    title: 'Movies & Shows',
+}
+
+
+export default function () {
+    return (
+        <>
+            <h1>Фильмы</h1>
+        </>
+    )
+}
