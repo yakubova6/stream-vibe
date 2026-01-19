9bff15a609e0181c0dd863965a4a272bc052f1fb
sanie <bitakov73@mail.ru>
2026-01-19 15:38:47 +0300

    feat: implement Header and Footer layouts
    - Build responsive Header with sticky animation, overlay menu, and mobile navigation
    - Create comprehensive Footer with multi-column grid menu and legal section
    - Add SCSS modules with responsive breakpoints, grid layouts, and animations
    - Include SVG icons for social media and UI elements

--- src/layouts/Footer/Footer.jsx
+++ src/layouts/Footer/Footer.jsx
@@ -1,5 +1,90 @@
+import './Footer.scss'
+import Socials from "@/components/Socials";
+
 const Footer = () => {
-    return <footer>FOOTER</footer>
+    const menuItems = [
+        {
+            title: 'Home',
+            links: ['Categories', 'Devices', 'Pricing', 'FAQ'],
+        },
+        {
+            title: 'Movies',
+            links: ['Gernes', 'Trending', 'New Release', 'Popular'],
+        },
+        {
+            title: 'Shows',
+            links: ['Gernes', 'Trending', 'New Release', 'Popular'],
+        },
+        {
+            title: 'Support',
+            links: ['Contact Us'],
+        },
+        {
+            title: 'Subscription',
+            links: ['Plans', 'Features'],
+        },
+        {
+            title: 'Connect With Us',
+            socialLinks: [
+                {
+                    label: 'Facebook',
+                    iconName: 'facebook',
+                },
+                {
+                    label: 'Twitter',
+                    iconName: 'twitter',
+                },
+                {
+                    label: 'LinkedIn',
+                    iconName: 'linked-in',
+                },
+            ]
+        },
+    ]
+
+    const extraLinks = ['Terms of Use', 'Privacy Policy', 'Cookie Policy']
+
+    return (
+        <footer className="footer">
+            <div className="footer__inner container">
+                <nav className="footer__menu">
+                    {menuItems.map(( { title, links, socialLinks }, index) => (
+                        <div className="footer__menu-column" key={index}>
+                            <a className="footer__menu-title h6" href="/">{title}</a>
+                            {links?.length > 0 && (
+                                <ul className="footer__menu-list">
+                                    {links.map((link, index) => (
+                                        <li className="footer__menu-item" key={index}>
+                                            <a className="footer__menu-link" href="/">
+                                                {link}
+                                            </a>
+                                        </li>
+                                    ))}
+                                </ul>
+                            )}
+                            {socialLinks?.length > 0 && (
+                                <Socials className="footer__soc1als" links={socialLinks} />
+                            )}
+                        </div>
+                    ))}
+                </nav>
+                <div className="footer__extra">
+                    <p className="footer__copyright">
+                        @<time dateTime="2023">2023</time> streamvib, All Rights Reserved
+                    </p>
+                    <ul className="footer__extra-list">
+                        {extraLinks.map((link, index) => (
+                            <li className="footer__extra-item" key={index}>
+                            <a className="footer__extra-link" href="/">
+                                {link}
+                            </a>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+        </footer>
+    )
 }
 
 export default Footer
--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -1,6 +1,8 @@
 import './Header.scss'
 import Logo from "@/components/Logo";
 import classNames from "classnames";
+import Button from "@/components/Button";
+import BurgerButton from "@/components/BurgerButton";
 
 const Header = (props) => {
     const {
@@ -23,34 +25,61 @@ const Header = (props) => {
         {
             label: 'Subscriptions',
             href: '/subscriptions',
-        }
+        },
     ]
 
     return (
-        <header className="header">
+        <header className="header" data-js-overlay-menu="">
             <div className="header__inner container">
                 <Logo
                     className="header__logo"
                     loading="eager"
                 />
-                <nav className="header__menu">
-                    <ul className="header__menu-list">
-                        {
-                            menuItems.map(({ label, href }, index) => (
-                                <li className="header__menu-item" key={index}>
-                                    <a
-                                        className={classNames('header__menu-link', {
-                                            'is-active': href === url
-                                        })}
-                                        href={href}
-                                    >
-                                        {label}
-                                    </a>
-                                </li>
-                            ))
-                        }
-                    </ul>
-                </nav>
+                <dialog
+                    className="header__overlay-menu-dialog"
+                    data-js-overlay-menu-dialog=""
+                >
+                    <nav className="header__menu">
+                        <ul className="header__menu-list">
+                            {
+                                menuItems.map(({label, href}, index) => (
+                                    <li className="header__menu-item" key={index}>
+                                        <a
+                                            className={classNames('header__menu-link', {
+                                                'is-active': href === url
+                                            })}
+                                            href={href}
+                                        >
+                                            {label}
+                                        </a>
+                                    </li>
+                                ))
+                            }
+                        </ul>
+                    </nav>
+                    <div className="header__actions">
+                        <Button
+                            className="header__button"
+                            label="Search"
+                            isLabelHidden
+                            mode="transparent"
+                            iconName="search"
+                        />
+                        <Button
+                            className="header__button"
+                            label="Notifications"
+                            isLabelHidden
+                            mode="transparent"
+                            iconName="notification"
+                        />
+                    </div>
+                </dialog>
+                <BurgerButton
+                    className="header__burger-button visible-tablet"
+                    extraAttrs={{
+                        'data-js-overlay-menu-burger-button': '',
+                    }}
+                />
             </div>
         </header>
     )
