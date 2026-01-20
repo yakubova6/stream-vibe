a3c1abecbb5c734bc3b68774cb848f2f0d3ceb3b
sanie <bitakov73@mail.ru>
2026-01-20 23:49:58 +0300

    feat: add Hero section and improve header flexibility
    - Create Hero section with panoramic background and play button
    - Add responsive SCSS for Hero with fluid typography and gradients
    - Update global.jsx to support isHeaderFixed prop for conditional header styling
    - Enhance Header component with isFixed prop support
    - Update Header SCSS with fixed position styling option
    - Configure home page with Hero section and fixed header metadata

--- src/global.jsx
+++ src/global.jsx
@@ -9,6 +9,7 @@ export default function (props) {
         children,
         title,
         url,
+        isHeaderFixed,
     } = props
 
     return (
@@ -21,7 +22,7 @@ export default function (props) {
                 <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
                 <link rel="manifest" href="/site.webmanifest"/>
             </Head>
-            <Header url={url} />
+            <Header url={url} isFixed={isHeaderFixed}/>
             <Content>
                 {children}
             </Content>
--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -6,7 +6,8 @@ import BurgerButton from "@/components/BurgerButton";
 
 const Header = (props) => {
     const {
-        url
+        url,
+        isFixed,
     } = props
 
     const menuItems = [
@@ -29,7 +30,12 @@ const Header = (props) => {
     ]
 
     return (
-        <header className="header" data-js-overlay-menu="">
+        <header
+            className={classNames('header', {
+                'is-fixed': isFixed,
+            })}
+            data-js-overlay-menu=""
+        >
             <div className="header__inner container">
                 <Logo
                     className="header__logo"
