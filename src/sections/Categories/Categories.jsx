2f79801bbe26fc19098ed7bab04ed3e9d7595478
sanie <bitakov73@mail.ru>
2026-01-27 15:37:05 +0300

    feat(sections): implement Categories section with interactive cards grid

--- src/sections/Categories/Categories.jsx
+++ src/sections/Categories/Categories.jsx
@@ -1,6 +1,55 @@
 import Section from "@/layouts/Section";
+import CategoryCard from "@/components/CategoryCard";
 
 const Categories = () => {
+    const categoryItems = [
+        {
+            title: 'Action',
+            images: [
+                '/src/assets/images/categories/action/1.jpg',
+                '/src/assets/images/categories/action/2.jpg',
+                '/src/assets/images/categories/action/3.jpg',
+                '/src/assets/images/categories/action/4.jpg',
+            ]
+        },
+        {
+            title: 'Adventure',
+            images: [
+                '/src/assets/images/categories/adventure/1.jpg',
+                '/src/assets/images/categories/adventure/2.jpg',
+                '/src/assets/images/categories/adventure/3.jpg',
+                '/src/assets/images/categories/adventure/4.jpg',
+            ]
+        },
+        {
+            title: 'Comedy',
+            images: [
+                '/src/assets/images/categories/comedy/1.jpg',
+                '/src/assets/images/categories/comedy/2.jpg',
+                '/src/assets/images/categories/comedy/3.jpg',
+                '/src/assets/images/categories/comedy/4.jpg',
+            ]
+        },
+        {
+            title: 'Drama',
+            images: [
+                '/src/assets/images/categories/drama/1.jpg',
+                '/src/assets/images/categories/drama/2.jpg',
+                '/src/assets/images/categories/drama/3.jpg',
+                '/src/assets/images/categories/drama/4.jpg',
+            ]
+        },
+        {
+            title: 'Horror',
+            images: [
+                '/src/assets/images/categories/horror/1.jpg',
+                '/src/assets/images/categories/horror/2.jpg',
+                '/src/assets/images/categories/horror/3.jpg',
+                '/src/assets/images/categories/horror/4.jpg',
+            ]
+        },
+    ]
+
     return (
         <Section
             title="Explore our wide variety of categories"
@@ -12,9 +61,14 @@ const Categories = () => {
                     <button>Вперед</button>
                 </div>
             )}
-            // isActionsHiddenOnMobile
+            isActionsHiddenOnMobile
         >
-            CATEGORIES
+            {categoryItems.map((categoryItem, index) => (
+                <CategoryCard
+                    {...categoryItem}
+                    key={index}
+                />
+            ))}
         </Section>
     )
 }
