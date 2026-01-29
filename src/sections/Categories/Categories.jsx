4d4fbea5534f7fa1b79788925c8374e98a1147e7
sanie <bitakov73@mail.ru>
2026-01-29 19:57:55 +0300

    refactor(categories): extract categories data to separate file
    - Move categoryItems array to categoryItems.js
    - Add new slide groups for categories
    - Improve data structure for reusability

--- src/sections/Categories/Categories.jsx
+++ src/sections/Categories/Categories.jsx
@@ -2,56 +2,9 @@ import Section from "@/layouts/Section";
 import CategoryCard from "@/components/CategoryCard";
 import Slider from "@/components/Slider";
 import SliderNavigation from "@/components/Slider/components/SliderNavigation";
+import categoryItems from "./categoryItems";
 
 const Categories = () => {
-    const categoryItems = [
-        {
-            title: 'Action',
-            images: [
-                '/src/assets/images/categories/action/1.jpg',
-                '/src/assets/images/categories/action/2.jpg',
-                '/src/assets/images/categories/action/3.jpg',
-                '/src/assets/images/categories/action/4.jpg',
-            ]
-        },
-        {
-            title: 'Adventure',
-            images: [
-                '/src/assets/images/categories/adventure/1.jpg',
-                '/src/assets/images/categories/adventure/2.jpg',
-                '/src/assets/images/categories/adventure/3.jpg',
-                '/src/assets/images/categories/adventure/4.jpg',
-            ]
-        },
-        {
-            title: 'Comedy',
-            images: [
-                '/src/assets/images/categories/comedy/1.jpg',
-                '/src/assets/images/categories/comedy/2.jpg',
-                '/src/assets/images/categories/comedy/3.jpg',
-                '/src/assets/images/categories/comedy/4.jpg',
-            ]
-        },
-        {
-            title: 'Drama',
-            images: [
-                '/src/assets/images/categories/drama/1.jpg',
-                '/src/assets/images/categories/drama/2.jpg',
-                '/src/assets/images/categories/drama/3.jpg',
-                '/src/assets/images/categories/drama/4.jpg',
-            ]
-        },
-        {
-            title: 'Horror',
-            images: [
-                '/src/assets/images/categories/horror/1.jpg',
-                '/src/assets/images/categories/horror/2.jpg',
-                '/src/assets/images/categories/horror/3.jpg',
-                '/src/assets/images/categories/horror/4.jpg',
-            ]
-        },
-    ]
-
     const sliderNavigationId = 'categories-slider-navigation'
     
     return (
@@ -60,12 +13,10 @@ const Categories = () => {
             titleId="categories-title"
             description="Whether you're looking for a comedy to make you laugh, a drama to make you think, or a documentary to learn something new"
             actions={(
-                <div>
                     <SliderNavigation
                         mode="tile"
                         id={sliderNavigationId}
                     />
-                </div>
             )}
             isActionsHiddenOnMobile
         >
---
+++ src/sections/Categories/categoryItems.js
@@ -0,0 +1,188 @@
+// Array of category items for sliders with images and titles
+const categoryItems = [
+    {
+        title: 'Action',
+        images: [
+            '/src/assets/images/categories/action/1.jpg',
+            '/src/assets/images/categories/action/2.jpg',
+            '/src/assets/images/categories/action/3.jpg',
+            '/src/assets/images/categories/action/4.jpg',
+        ]
+    },
+    {
+        title: 'Adventure',
+        images: [
+            '/src/assets/images/categories/adventure/1.jpg',
+            '/src/assets/images/categories/adventure/2.jpg',
+            '/src/assets/images/categories/adventure/3.jpg',
+            '/src/assets/images/categories/adventure/4.jpg',
+        ]
+    },
+    {
+        title: 'Comedy',
+        images: [
+            '/src/assets/images/categories/comedy/1.jpg',
+            '/src/assets/images/categories/comedy/2.jpg',
+            '/src/assets/images/categories/comedy/3.jpg',
+            '/src/assets/images/categories/comedy/4.jpg',
+        ]
+    },
+    {
+        title: 'Drama',
+        images: [
+            '/src/assets/images/categories/drama/1.jpg',
+            '/src/assets/images/categories/drama/2.jpg',
+            '/src/assets/images/categories/drama/3.jpg',
+            '/src/assets/images/categories/drama/4.jpg',
+        ]
+    },
+    {
+        title: 'Horror',
+        images: [
+            '/src/assets/images/categories/horror/1.jpg',
+            '/src/assets/images/categories/horror/2.jpg',
+            '/src/assets/images/categories/horror/3.jpg',
+            '/src/assets/images/categories/horror/4.jpg',
+        ]
+    },
+
+    {
+        title: 'Action',
+        images: [
+            '/src/assets/images/categories/action/1.jpg',
+            '/src/assets/images/categories/action/2.jpg',
+            '/src/assets/images/categories/action/3.jpg',
+            '/src/assets/images/categories/action/4.jpg',
+        ]
+    },
+    {
+        title: 'Adventure',
+        images: [
+            '/src/assets/images/categories/adventure/1.jpg',
+            '/src/assets/images/categories/adventure/2.jpg',
+            '/src/assets/images/categories/adventure/3.jpg',
+            '/src/assets/images/categories/adventure/4.jpg',
+        ]
+    },
+    {
+        title: 'Comedy',
+        images: [
+            '/src/assets/images/categories/comedy/1.jpg',
+            '/src/assets/images/categories/comedy/2.jpg',
+            '/src/assets/images/categories/comedy/3.jpg',
+            '/src/assets/images/categories/comedy/4.jpg',
+        ]
+    },
+    {
+        title: 'Drama',
+        images: [
+            '/src/assets/images/categories/drama/1.jpg',
+            '/src/assets/images/categories/drama/2.jpg',
+            '/src/assets/images/categories/drama/3.jpg',
+            '/src/assets/images/categories/drama/4.jpg',
+        ]
+    },
+    {
+        title: 'Horror',
+        images: [
+            '/src/assets/images/categories/horror/1.jpg',
+            '/src/assets/images/categories/horror/2.jpg',
+            '/src/assets/images/categories/horror/3.jpg',
+            '/src/assets/images/categories/horror/4.jpg',
+        ]
+    },
+
+    {
+        title: 'Action',
+        images: [
+            '/src/assets/images/categories/action/1.jpg',
+            '/src/assets/images/categories/action/2.jpg',
+            '/src/assets/images/categories/action/3.jpg',
+            '/src/assets/images/categories/action/4.jpg',
+        ]
+    },
+    {
+        title: 'Adventure',
+        images: [
+            '/src/assets/images/categories/adventure/1.jpg',
+            '/src/assets/images/categories/adventure/2.jpg',
+            '/src/assets/images/categories/adventure/3.jpg',
+            '/src/assets/images/categories/adventure/4.jpg',
+        ]
+    },
+    {
+        title: 'Comedy',
+        images: [
+            '/src/assets/images/categories/comedy/1.jpg',
+            '/src/assets/images/categories/comedy/2.jpg',
+            '/src/assets/images/categories/comedy/3.jpg',
+            '/src/assets/images/categories/comedy/4.jpg',
+        ]
+    },
+    {
+        title: 'Drama',
+        images: [
+            '/src/assets/images/categories/drama/1.jpg',
+            '/src/assets/images/categories/drama/2.jpg',
+            '/src/assets/images/categories/drama/3.jpg',
+            '/src/assets/images/categories/drama/4.jpg',
+        ]
+    },
+    {
+        title: 'Horror',
+        images: [
+            '/src/assets/images/categories/horror/1.jpg',
+            '/src/assets/images/categories/horror/2.jpg',
+            '/src/assets/images/categories/horror/3.jpg',
+            '/src/assets/images/categories/horror/4.jpg',
+        ]
+    },
+
+    {
+        title: 'Action',
+        images: [
+            '/src/assets/images/categories/action/1.jpg',
+            '/src/assets/images/categories/action/2.jpg',
+            '/src/assets/images/categories/action/3.jpg',
+            '/src/assets/images/categories/action/4.jpg',
+        ]
+    },
+    {
+        title: 'Adventure',
+        images: [
+            '/src/assets/images/categories/adventure/1.jpg',
+            '/src/assets/images/categories/adventure/2.jpg',
+            '/src/assets/images/categories/adventure/3.jpg',
+            '/src/assets/images/categories/adventure/4.jpg',
+        ]
+    },
+    {
+        title: 'Comedy',
+        images: [
+            '/src/assets/images/categories/comedy/1.jpg',
+            '/src/assets/images/categories/comedy/2.jpg',
+            '/src/assets/images/categories/comedy/3.jpg',
+            '/src/assets/images/categories/comedy/4.jpg',
+        ]
+    },
+    {
+        title: 'Drama',
+        images: [
+            '/src/assets/images/categories/drama/1.jpg',
+            '/src/assets/images/categories/drama/2.jpg',
+            '/src/assets/images/categories/drama/3.jpg',
+            '/src/assets/images/categories/drama/4.jpg',
+        ]
+    },
+    {
+        title: 'Horror',
+        images: [
+            '/src/assets/images/categories/horror/1.jpg',
+            '/src/assets/images/categories/horror/2.jpg',
+            '/src/assets/images/categories/horror/3.jpg',
+            '/src/assets/images/categories/horror/4.jpg',
+        ]
+    },
+]
+
+export default categoryItems
